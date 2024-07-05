58d1e8d60cc16aa00a518c6c1ce6bff63e5eb0b3
sandybhatta <105071143+sandybhatta@users.noreply.github.com>
2024-07-05 22:04:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let lists=document.querySelectorAll("li");
+let count=0;
+for(let i=0;i<lists.length;i++){
+	if(lists[i].id=="level"){
+		alert(`The level of the element is: ${count}`)
+	}
+	else{
+		count++;
+	}
+}
